20e01e423b21e84016ce0d57898d1da570ec7b1e
krish-98 <muralikrishnanm98@gmail.com>
2023-11-04 16:54:36 +0530

    create a test route

---
+++ api/controllers/authController.js
@@ -0,0 +1,18 @@
+import User from "../models/User.js"
+import bcrypt from "bcrypt"
+
+export const signup = async (req, res) => {
+  try {
+    const { email, username, password } = req.body
+    const hashedPassword = bcrypt.hashSync(password, 10)
+    const newUser = new User({ email, username, password: hashedPassword })
+    await newUser.save()
+
+    if (!newUser) {
+      return res.status(403).json({ message: "User not added" })
+    }
+    res.status(201).json({ message: "User created successfully" })
+  } catch (error) {
+    res.status.json({ message: error.message })
+  }
+}
--- api/index.js
+++ api/index.js
@@ -1,22 +1,20 @@
 import express from "express"
 import mongoose from "mongoose"
 import dotenv from "dotenv"
+import userRouter from "./routes/userRoutes.js"
 dotenv.config()
 
 const app = express()
 const PORT = process.env.PORT || 3000
 
+app.use(express.json())
+app.use("/api/user", userRouter)
+
 mongoose
   .connect(process.env.MONGODB_URI)
-  .then(() =>
-    app.listen(3000, () => {
-      console.log(
-        `Connected to MongoDB database and the Server is running on port ${PORT}`
-      )
-    })
-  )
+  .then(() => console.log("Database connection established"))
   .catch((err) => console.log(err))
 
-app.get("/", (req, res) => {
-  res.json({ message: "Hello World" })
+app.listen(3000, () => {
+  console.log(`Server is running on port ${PORT}`)
 })
