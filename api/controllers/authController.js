399e8f67275be1f26536784e8c045138c0019aa7
krish-98 <muralikrishnanm98@gmail.com>
2023-11-05 23:39:29 +0530

    create sign in route

--- api/controllers/authController.js
+++ api/controllers/authController.js
@@ -1,5 +1,7 @@
+import { errorHandler } from "../error.js"
 import User from "../models/User.js"
 import bcrypt from "bcrypt"
+import jwt from "jsonwebtoken"
 
 export const signup = async (req, res, next) => {
   try {
@@ -12,6 +14,25 @@ export const signup = async (req, res, next) => {
       return res.status(403).json({ message: "User not added" })
     }
     res.status(201).json({ message: "User created successfully" })
+  } catch (error) {
+    next(error)
+  }
+}
+
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body
+
+  try {
+    const validUser = await User.findOne({ email })
+    if (!validUser) return next(errorHandler(404, "User not found!"))
+    const validPassword = bcrypt.compareSync(password, validUser.password)
+    if (!validPassword) return next(errorHandler(401, "Wrong crendetials!"))
+
+    // Destructing the object to avoid sending the password to the client
+    const { password: pass, ...rest } = validUser._doc
+
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET)
+    res.cookie("access_token", token, { httpOnly: true }).status(200).json(rest)
   } catch (error) {
     next(error)
   }
