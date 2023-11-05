baf201d07d53dc28bc97edeca94144fa7e8d89db
krish-98 <muralikrishnanm98@gmail.com>
2023-11-05 13:36:43 +0530

    complete sign up page functionality

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from "express"
 import mongoose from "mongoose"
 import dotenv from "dotenv"
+import cors from "cors"
 import authRouter from "./routes/authRoutes.js"
 import userRouter from "./routes/userRoutes.js"
 dotenv.config()
@@ -8,13 +9,16 @@ dotenv.config()
 const app = express()
 const PORT = process.env.PORT || 3000
 
+// app.use(cors())
 app.use(express.json())
 app.use("/api/auth", authRouter)
 app.use("/api/user", userRouter)
+
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500
   const message = err.message || "Internal Server Error"
-  return res.status(statusCode).json({ sucess: false, statusCode, message })
+
+  return res.status(statusCode).json({ success: false, statusCode, message })
 })
 
 mongoose
--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,30 +1,76 @@
-import { Link } from "react-router-dom"
+import { useState } from "react"
+import { Link, useNavigate } from "react-router-dom"
 
 export default function SignUp() {
+  const [formData, setFormData] = useState({})
+  const [error, setError] = useState(null)
+  const [isLoading, setIsLoading] = useState(false)
+  const navigate = useNavigate()
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value })
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    try {
+      setIsLoading(true)
+      const response = await fetch("/api/auth/signup", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(formData),
+      })
+      const data = await response.json()
+
+      if (data.success === false) {
+        setIsLoading(false)
+        setError(data.message)
+        return
+      }
+      setIsLoading(false)
+      setError(null)
+      navigate("/sign-in")
+      console.log(data)
+    } catch (error) {
+      setIsLoading(false)
+      setError(error.message)
+    }
+  }
+
   return (
     <div className="p-4 max-w-lg mx-auto pt-36">
       <h1 className="text-3xl text-center font-semibold my-7">Sign Up</h1>
-      <form className="flex flex-col gap-4">
+      <form onSubmit={handleSubmit} className="flex flex-col gap-4">
         <input
           type="text"
           placeholder="username"
-          className="border p-3 rounded-lg"
           id="username"
+          required
+          className="border p-3 rounded-lg"
+          onChange={handleChange}
         />
         <input
           type="email"
           placeholder="email"
-          className="border p-3 rounded-lg"
           id="email"
+          required
+          className="border p-3 rounded-lg"
+          onChange={handleChange}
         />
         <input
           type="password"
           placeholder="password"
-          className="border p-3 rounded-lg"
           id="password"
+          required
+          className="border p-3 rounded-lg"
+          onChange={handleChange}
         />
-        <button className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-90 disabled:opacity-70">
-          Sign
+        <button
+          disabled={isLoading}
+          type="submit"
+          className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-90 disabled:opacity-70"
+        >
+          {isLoading ? "Loading..." : "Sign Up"}
         </button>
       </form>
 
@@ -34,6 +80,7 @@ export default function SignUp() {
           <span className="text-blue-700 hover:underline">Sign In</span>
         </Link>
       </div>
+      {error ? <p className="text-red-600 font-medium my-2">{error}</p> : ""}
     </div>
   )
 }
