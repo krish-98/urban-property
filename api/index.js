6fa802f784e975c4faca1919c7ac5d7f46e00d7a
krish-98 <muralikrishnanm98@gmail.com>
2023-11-27 23:10:59 +0530

    add pre installation

--- api/index.js
+++ api/index.js
@@ -5,8 +5,10 @@ import authRouter from "./routes/authRoutes.js"
 import userRouter from "./routes/userRoutes.js"
 import listingRouter from "./routes/listingRoutes.js"
 import cookieParser from "cookie-parser"
+import path from "path"
 dotenv.config()
 
+const __dirname = path.resolve()
 const app = express()
 const PORT = process.env.PORT || 3000
 
@@ -16,6 +18,12 @@ app.use(cookieParser())
 app.use("/api/auth", authRouter)
 app.use("/api/user", userRouter)
 app.use("/api/listing", listingRouter)
+
+app.use(express.static(path.join(__dirname, "/client/dist")))
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"))
+})
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500
