3fb326e2dcf581fe37cd5d099aeff6d36ca045d8
krish-98 <muralikrishnanm98@gmail.com>
2024-05-15 15:10:50 +0530

    complete about design page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -20,12 +20,10 @@ export default function Home() {
   const [saleListings, setSaleListings] = useState([])
   const [rentListings, setRentListings] = useState([])
 
-  SwiperCore.use([Navigation])
-
   useEffect(() => {
     const fetchOfferListings = async () => {
       try {
-        const res = await fetch('/api/listing/get?offer=true&limit=4')
+        const res = await fetch('/api/listing/get?offer=true&limit=3')
         const data = await res.json()
         setOfferListings(data)
 
@@ -37,7 +35,7 @@ export default function Home() {
 
     const fetchRentListings = async () => {
       try {
-        const res = await fetch('/api/listing/get?type=rent&limit=4')
+        const res = await fetch('/api/listing/get?type=rent&limit=3')
         const data = await res.json()
         setRentListings(data)
 
@@ -49,7 +47,7 @@ export default function Home() {
 
     const fetchSaleListings = async () => {
       try {
-        const res = await fetch('/api/listing/get?type=sale&limit=4')
+        const res = await fetch('/api/listing/get?type=sale&limit=3')
         const data = await res.json()
         setSaleListings(data)
       } catch (error) {
@@ -59,6 +57,7 @@ export default function Home() {
 
     fetchOfferListings()
   }, [])
+
   return (
     <>
       {/* Header */}
@@ -94,7 +93,7 @@ export default function Home() {
 
       {/* Mid */}
       <section>
-        <div className="max-w-5xl mx-auto">
+        <div className="max-w-6xl mx-auto">
           <div className="flex flex-col md:flex-row items-center justify-center gap-4 md:gap-6 my-10 md:my-16">
             <div className="bg-[#ffe0ce] text-black py-16 pl-8 pr-16 rounded-xl space-y-4 lg:space-y-7 md:py-24">
               <h1 className="font-semibold text-xl lg:text-3xl">
@@ -134,8 +133,8 @@ export default function Home() {
         </div>
 
         <div className="bg-[#fffaf7]">
-          <div className="max-w-5xl mx-auto flex flex-col md:flex-row items-center justify-center gap-8 md:gap-16 py-14 xl:justify-between">
-            <div className="space-y-1">
+          <div className="max-w-6xl mx-auto flex flex-col md:flex-row items-center justify-center gap-8 md:px-8 lg:gap-16 py-14 xl:justify-between">
+            <div className="space-y-1 w-[200px] h-[150px]">
               <AiOutlineDollar size={25} className="fill-[#fc670f]" />
               <p className="font-semibold text-lg md:text-xl lg:text-2xl">
                 5.4M
@@ -143,7 +142,8 @@ export default function Home() {
               <p className="text-slate-600 text-sm ">Owned from</p>
               <p className="text-slate-600 text-sm ">Properties tranactions</p>
             </div>
-            <div className="space-y-1">
+
+            <div className="space-y-1 w-[200px] h-[150px]">
               <FaMapLocationDot size={25} className="fill-[#fc670f]" />
               <p className="font-semibold text-lg md:text-xl lg:text-2xl">
                 25k+
@@ -153,31 +153,100 @@ export default function Home() {
               </p>
               <p className="text-slate-600 text-sm ">Successfully</p>
             </div>
-            <div className="space-y-1">
+
+            <div className="space-y-1 w-[200px] h-[150px]">
               <FaFireAlt size={25} className="fill-[#fc670f]" />
               <p className="font-semibold text-lg md:text-xl lg:text-2xl">
                 500
               </p>
               <p className="text-slate-600 text-sm ">Daily completed</p>
-              <p className="text-slate-600 text-sm ">tranactions</p>
+              <p className="text-slate-600 text-sm ">transactions</p>
             </div>
-            <div className="space-y-1">
+
+            <div className="space-y-1 w-[200px] h-[150px]">
               <PiMaskHappy size={25} className="fill-[#fc670f]" />
               <p className="font-semibold text-lg md:text-xl lg:text-2xl">
                 600
               </p>
-              <p className="text-slate-600 text-sm ">Regular Clients</p>
-              <p className="text-slate-600 text-sm "></p>
+              <p className="text-slate-600 text-sm">Regular Clients</p>
+              <p className="text-slate-600 text-sm"></p>
             </div>
           </div>
         </div>
       </section>
 
       {/* Featured Properties */}
-      <main></main>
+      <main>
+        {/* Listing results for Offer, sale and rent */}
+        <div className="max-w-6xl mx-auto p-3 flex flex-col gap-8 my-10 px-6">
+          <h3 className="text-2xl font-bold">Feaured Properties</h3>
+          {offerListings && offerListings.length > 0 && (
+            <div>
+              <div className="my-2">
+                <h2 className="text-xl font-semibold text-slate-600">
+                  Recent offers
+                </h2>
+                <Link
+                  className="text-sm font-semibold text-[#fb923c] hover:underline"
+                  to={'/search?offer=true'}
+                >
+                  Show more
+                </Link>
+              </div>
+              <div className="flex flex-wrap gap-4 xl:justify-between">
+                {offerListings.map((listing) => (
+                  <ListingItem listing={listing} key={listing._id} />
+                ))}
+              </div>
+            </div>
+          )}
+
+          {rentListings && rentListings.length > 0 && (
+            <div>
+              <div className="my-2">
+                <h2 className="text-xl font-semibold text-slate-600">
+                  Recent Rent Properties
+                </h2>
+                <Link
+                  className="text-sm font-semibold text-[#fb923c] hover:underline"
+                  to={'/search?offer=true'}
+                >
+                  Show more
+                </Link>
+              </div>
+              <div className="flex flex-wrap gap-4 xl:justify-between">
+                {rentListings.map((listing) => (
+                  <ListingItem listing={listing} key={listing._id} />
+                ))}
+              </div>
+            </div>
+          )}
+
+          {saleListings && saleListings.length > 0 && (
+            <div>
+              <div className="my-2">
+                <h2 className="text-xl font-semibold text-slate-600">
+                  Recent Sale Properties
+                </h2>
+                <Link
+                  className="text-sm font-semibold text-[#fb923c] hover:underline"
+                  to={'/search?offer=true'}
+                >
+                  Show more
+                </Link>
+              </div>
+              <div className="flex flex-wrap gap-4 xl:justify-between">
+                {saleListings.map((listing) => (
+                  <ListingItem listing={listing} key={listing._id} />
+                ))}
+              </div>
+            </div>
+          )}
+        </div>
+      </main>
 
       {/* Footer */}
-      <footer>
+      <footer className="bg-[#fff7f2]">
         <div className="max-w-6xl mx-auto flex flex-col lg:flex-row items-center justify-center py-20 gap-10 px-10">
           <div className="space-y-1 w-[300px] h-[200px]">
             <Link to="/">
@@ -221,111 +290,46 @@ export default function Home() {
   )
 }
 
-// return (
-//   <div className="bg-[#fff7f2]">
-//     {/* Top Section */}
-//     <div className="flex flex-col gap-6 p-28 px-3 max-w-6xl mx-auto">
-//       <h1 className="text-slate-700 font-bold text-3xl lg:text-6xl">
-//         Find your next <span className="text-slate-500">perfect</span>
-//         <br />
-//         place with ease
-//       </h1>
-//       <div className="text-gray-400 text-sm sm:text-sm">
-//         Urban Property is the best place to find your next perfect place to
-//         live.
-//         <br />
-//         We have a wide range of properties for you to choose from.
-//       </div>
-//       <Link
-//         to={'/search'}
-//         className="text-xs sm:text-sm text-blue-800 font-bold hover:underline"
-//       >
-//         Let&apos;s get started...
-//       </Link>
-//     </div>
+//  return (
+//    <div className="bg-[#fff7f2]">
+//      {/* Top Section */}
+//      <div className="flex flex-col gap-6 p-28 px-3 max-w-6xl mx-auto">
+//        <h1 className="text-slate-700 font-bold text-3xl lg:text-6xl">
+//          Find your next <span className="text-slate-500">perfect</span>
+//          <br />
+//          place with ease
+//        </h1>
+//        <div className="text-gray-400 text-sm sm:text-sm">
+//          Urban Property is the best place to find your next perfect place to
+//          live.
+//          <br />
+//          We have a wide range of properties for you to choose from.
+//        </div>
+//        <Link
+//          to={'/search'}
+//          className="text-xs sm:text-sm text-blue-800 font-bold hover:underline"
+//        >
+//          Let&apos;s get started...
+//        </Link>
+//      </div>
 
-//     {/* Swiper Section */}
-//     <Swiper navigation>
-//       {offerListings &&
-//         offerListings.length > 0 &&
-//         offerListings.map((listing, i) => (
-//           <SwiperSlide key={i}>
-//             <div
-//               style={{
-//                 background: `url(${listing.imageUrls[0]}) center no-repeat`,
-//                 backgroundSize: 'cover',
-//               }}
-//               className="h-[500px]"
-//               key={listing._id}
-//             ></div>
-//           </SwiperSlide>
-//         ))}
-//     </Swiper>
+//      {/* Swiper Section */}
+//      <Swiper navigation>
+//        {offerListings &&
+//          offerListings.length > 0 &&
+//          offerListings.map((listing, i) => (
+//            <SwiperSlide key={i}>
+//              <div
+//                style={{
+//                  background: `url(${listing.imageUrls[0]}) center no-repeat`,
+//                  backgroundSize: 'cover',
+//                }}
+//                className="h-[500px]"
+//                key={listing._id}
+//              ></div>
+//            </SwiperSlide>
+//          ))}
+//      </Swiper>
 
-//     {/* Listing results for Offer, sale and rent */}
-//     <div className="max-w-6xl mx-auto p-3 flex flex-col gap-8 my-10">
-//       {offerListings && offerListings.length > 0 && (
-//         <div>
-//           <div className="my-3">
-//             <h2 className="text-2xl font-semibold text-slate-600">
-//               Recent offers
-//             </h2>
-//             <Link
-//               className="text-sm text-blue-800 hover:underline"
-//               to={'/search?offer=true'}
-//             >
-//               Show more offers
-//             </Link>
-//           </div>
-//           <div className="flex flex-wrap gap-4">
-//             {offerListings.map((listing) => (
-//               <ListingItem listing={listing} key={listing._id} />
-//             ))}
-//           </div>
-//         </div>
-//       )}
-
-//       {rentListings && rentListings.length > 0 && (
-//         <div>
-//           <div className="my-3">
-//             <h2 className="text-2xl font-semibold text-slate-600">
-//               Recent offers
-//             </h2>
-//             <Link
-//               className="text-sm text-blue-800 hover:underline"
-//               to={'/search?offer=true'}
-//             >
-//               Recent places for rent
-//             </Link>
-//           </div>
-//           <div className="flex flex-wrap gap-4">
-//             {rentListings.map((listing) => (
-//               <ListingItem listing={listing} key={listing._id} />
-//             ))}
-//           </div>
-//         </div>
-//       )}
-
-//       {saleListings && saleListings.length > 0 && (
-//         <div>
-//           <div className="my-3">
-//             <h2 className="text-2xl font-semibold text-slate-600">
-//               Recent offers
-//             </h2>
-//             <Link
-//               className="text-sm text-blue-800 hover:underline"
-//               to={'/search?offer=true'}
-//             >
-//               Show more offers
-//             </Link>
-//           </div>
-//           <div className="flex flex-wrap gap-4">
-//             {saleListings.map((listing) => (
-//               <ListingItem listing={listing} key={listing._id} />
-//             ))}
-//           </div>
-//         </div>
-//       )}
-//     </div>
-//   </div>
-// )
+//    </div>
+//  )
