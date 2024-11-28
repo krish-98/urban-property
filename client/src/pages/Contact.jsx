f2393cdbc298d49f07dc4120817cf696056883b9
krish-98 <muralikrishnanm98@gmail.com>
2024-11-28 14:32:38 +0530

    Update only signin user can contact owner

--- client/src/pages/Contact.jsx
+++ client/src/pages/Contact.jsx
@@ -45,57 +45,51 @@ export default function Contact({ listing }) {
                   <p className="text-sm text-gray-500 md:text-base">
                     {landlord?.email}
                   </p>
-                  {!currentUser && (
-                    <Link
-                      to={'/sign-in'}
-                      className="bg-ubOrange text-white text-center font-semibold px-4 py-2 rounded-xl hover:scale-105 hover:bg-black transition-all duration-300"
-                    >
-                      Sign In to contact
-                    </Link>
-                  )}
                 </div>
               </div>
             </div>
 
-            <div className="flex flex-col gap-3 w-full">
-              <p className="text-sm lg:text-base text-gray-500">
-                Contact{' '}
-                <span className="text-ubOrange font-semibold capitalize">
-                  {landlord.username}
-                </span>{' '}
-                for{' '}
-                <span className="text-ubOrange font-semibold capitalize">
-                  {listing.name}
-                </span>
-              </p>
-              <textarea
-                name="message"
-                id="message"
-                rows="2"
-                value={message}
-                onChange={handleOnChange}
-                placeholder="Enter your message here..."
-                className="w-full border p-3 rounded-lg"
-              ></textarea>
-
+            {!currentUser ? (
               <Link
-                onClick={handleSendMessage}
-                to={`mailto:${landlord.email}?subject=Regarding ${listing.name}&body=${message}`}
-                className="bg-ubOrange text-white text-center p-3 uppercase rounded-lg hover:bg-orange-500"
+                to={'/sign-in'}
+                className="bg-ubOrange text-white text-center font-semibold px-4 py-2 md:py-3 lg:py-4 rounded-xl hover:scale-105 hover:bg-black transition-all duration-300 w-full"
               >
-                Send Message
+                Sign in to contact
               </Link>
-            </div>
+            ) : (
+              <div className="flex flex-col gap-3 w-full">
+                <p className="text-sm lg:text-base text-gray-500">
+                  Contact{' '}
+                  <span className="text-ubOrange font-semibold capitalize">
+                    {landlord.username}
+                  </span>{' '}
+                  for{' '}
+                  <span className="text-ubOrange font-semibold capitalize">
+                    {listing.name}
+                  </span>
+                </p>
+                <textarea
+                  name="message"
+                  id="message"
+                  rows="2"
+                  value={message}
+                  onChange={handleOnChange}
+                  placeholder="Enter your message here..."
+                  className="w-full border p-3 rounded-lg"
+                ></textarea>
+
+                <Link
+                  onClick={handleSendMessage}
+                  to={`mailto:${landlord.email}?subject=Regarding ${listing.name}&body=${message}`}
+                  className="bg-ubOrange text-white text-center p-3 uppercase rounded-lg hover:bg-orange-500"
+                >
+                  Send Message
+                </Link>
+              </div>
+            )}
           </div>
         </div>
       )}
     </>
   )
 }
-
-// <Link
-//   to={`mailto:${landlord?.email}?subject=Regarding this property&body=I'm contacting you regarding ${landlord?.username} property information`}
-//   className="bg-ubOrange text-white text-center font-semibold uppercase px-4 py-2 rounded-xl hover:scale-105 hover:bg-black transition-all duration-300"
-// >
-//   Contact
-// </Link>
