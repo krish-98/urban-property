8e21bac15a5528a9830c31dbead2a42949f04209
krish-98 <muralikrishnanm98@gmail.com>
2024-12-01 14:10:48 +0530

    Update completely move the frontend to TS

--- client/src/components/SuspenseWrapper.jsx
+++
@@ -1,6 +0,0 @@
-import { Suspense } from 'react'
-import SuspenseLoader from './fallback-ui/SuspenseLoader'
-
-export default function SuspenseWrapper({ component }) {
-  return <Suspense fallback={<SuspenseLoader />}>{component}</Suspense>
-}
