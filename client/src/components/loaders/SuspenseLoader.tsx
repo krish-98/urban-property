5b9634303335e7afecd2619528e926059235bb76
krish-98 <muralikrishnanm98@gmail.com>
2026-01-01 22:06:00 +0530

    remove all folders

--- client/src/components/loaders/SuspenseLoader.tsx
+++
@@ -1,9 +0,0 @@
-import { HashLoader } from 'react-spinners'
-
-export default function SuspenseLoader() {
-  return (
-    <div className="min-h-[calc(100vh-72px)] flex items-center justify-center bg-mainColor">
-      <HashLoader color="#fb923c" />
-    </div>
-  )
-}
