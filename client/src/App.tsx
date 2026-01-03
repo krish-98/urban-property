0b90df0216fb300c60a818921629c4dbf5c90e5a
krish-98 <muralikrishnanm98@gmail.com>
2026-01-03 13:11:15 +0530

    Add tailwindCSS & motion

---
+++ client/src/App.tsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <div className="h-screen bg-slate-900">
+      <h1 className="text-3xl text-cyan-200 font-bold">Urban Property</h1>
+    </div>
+  )
+}
